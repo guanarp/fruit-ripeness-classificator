308d036bb14bea618536c713e852e2bf9444b167
guanarp <jriosparquet@gmail.com>
2024-10-16 10:14:06 -0300

    deleted files for restructure

--- pc/drive_auth_getter.py
+++
@@ -1,6 +0,0 @@
-from pydrive2.auth import GoogleAuth
-
-gauth = GoogleAuth()
-gauth.OAuthSettingsFile = "client_secrets.json"
-gauth.LocalWebserverAuth()  # This will open a web browser for authentication
-gauth.SaveCredentialsFile("credentials.json")
